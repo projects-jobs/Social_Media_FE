17a5e4b4119ad451e68a3a4f15b3fa5ed11e56a8
subathra2000mdu <subathra2000mdu@gmail.com>
2026-04-16 09:17:01 +0530

    code changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js  ─ constants only
-export const API_BASE = "http://localhost:5000/api";
-export const BASE_IMG = "http://localhost:5000/images/";
+export const API_BASE = "https://social-media-be-jzvd.onrender.com/api";
+export const BASE_IMG = "https://social-media-be-jzvd.onrender.com/images/";
 
 export const imgUrl   = (f)    => f ? `${BASE_IMG}${f}` : null;
 export const avatarUrl= (name = "User") =>
